8f8dee34a0d23869d042072356ccd3d47669c861
Diego Lao <howarto@ymail.com>
2017-03-04 12:33:51 +0100

    config for azure

--- main.js
+++ main.js
@@ -7,8 +7,13 @@ var restify = require('restify');
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+server.post('/api/messages', connector.listen());
+server.get(/.*/, restify.serveStatic({
+    'directory': '.',
+    'default': 'index.html'
+}));
+server.listen(process.env.port || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url); 
 });
   
 // Create chat bot
@@ -18,7 +23,6 @@ var connector = new builder.ChatConnector({
 });
 
 var bot = new builder.UniversalBot(connector);
-server.post('/api/messages', connector.listen());
 
 //=========================================================
 // Bots Dialogs
