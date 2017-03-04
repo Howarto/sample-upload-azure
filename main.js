ffb36a07a0341330f8153fbf25700821321ba60c
Diego Lao <howarto@ymail.com>
2017-03-04 14:28:53 +0100

    config for azure

--- main.js
+++ main.js
@@ -1,76 +1,34 @@
-var builder = require('botbuilder');
 var restify = require('restify');
+var builder = require('botbuilder');
 
-//=========================================================
-// Bot Setup
-//=========================================================
-
-// Create chat bot
-var connector = new builder.ChatConnector({
-    appId: process.env.BOTFRAMEWORK_APPID,
+// Get secrets from server environment
+var botConnectorOptions = { 
+    appId: process.env.BOTFRAMEWORK_APPID, 
     appPassword: process.env.BOTFRAMEWORK_APPSECRET
-});
+};
 
+// Create bot
+var connector = new builder.ChatConnector(botConnectorOptions);
 var bot = new builder.UniversalBot(connector);
+
+bot.dialog('/', function (session) {
+    
+    //respond with user's message
+    session.send("You said " + session.message.text);
+});
 
 // Setup Restify Server
 var server = restify.createServer();
+
+// Handle Bot Framework messages
 server.post('/api/messages', connector.listen());
+
+// Serve a static web page
 server.get(/.*/, restify.serveStatic({
     'directory': '.',
     'default': 'index.html'
 }));
+
 server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
-  
-
-//=========================================================
-// Bots Dialogs
-//=========================================================
-
-// bot.dialog('/', function (session) {
-//     session.send("Hello World");
-// });
-
-
-var intents = new builder.IntentDialog();
-bot.dialog('/', [
-    function (session) {
-        session.send("Good. First of all choose the image that seems like your burn.");
-        var msg = new builder.Message(session)
-            .textFormat(builder.TextFormat.xml)
-            .attachmentLayout('carousel')   // Horizontal to swipe!
-            .attachments([
-                new builder.HeroCard(session)
-                    .title("First degree burn")
-                    .images([builder.CardImage.create(session, "http://i67.tinypic.com/j63ps0.jpg")])
-                    .tap(builder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/Space_Needle")),
-            
-                new builder.HeroCard(session)
-                    .title("Second degree burn")
-                    .images([builder.CardImage.create(session, "http://i68.tinypic.com/34rjcqs.jpg")])
-                    .tap(builder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/Space_Needle")),
-
-            new builder.HeroCard(session)
-                    .title("Second degree burn")
-                    .images([builder.CardImage.create(session, "http://i65.tinypic.com/1zfk1v5.jpg")])
-                    .tap(builder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/Space_Needle")),
-
-            new builder.HeroCard(session)
-                    .title("Thirst degree burn")
-                    .images([builder.CardImage.create(session, "http://i63.tinypic.com/14jumwh.jpg")])
-                    .tap(builder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/Space_Needle"))
-        
-        ]);
-        session.send(msg);
-        session.beginDialog('/otro');
-    }
-]);
-
-bot.dialog('/otro', [
-    function (session) {
-        session.send("Hi");
-        session.endDialog(msg);
-    }
-]);
